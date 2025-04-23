f5fb2fff7e04e8508af52894457b7e9667c47226
YangSeungHyunn <yangsh5972@gmail.com>
2025-04-23 19:26:09 +0900

    메소드 안 CSV 불러오기 삭제

--- R_Restaurant.java
+++ R_Restaurant.java
@@ -19,13 +19,11 @@ public class R_Restaurant {
 
 	// 1. 전체 목록 조회
 	public List<Restaurant> doRetrieve() {
-		ReadCSVfile_Edit.loadRestaurants(ReadCSVfile_Edit.Restaurant_DATA);// 콘솔 실행 시 CSV 불러오기 하면 삭제 가능
 		return ReadCSVfile_Edit.getrestaurantList();
 	}
 
 	// 2. 단건 목록 조회
 	public Restaurant doSelectOne(String resName) {
-		ReadCSVfile_Edit.loadRestaurants(ReadCSVfile_Edit.Restaurant_DATA); // 콘솔 실행 시 CSV 불러오기 하면 삭제 가능
 		return ReadCSVfile_Edit.getRestaurantMap().get(resName);
 	}
 }
