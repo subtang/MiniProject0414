ba038a744a4233b10e64545a99783b86bd9f093f
ebh99 <bh990907@naver.com>
2025-04-23 19:58:53 +0900

    C,U Restaurant 코드 수정(오류확인해서 고치기 요망)

---
+++ C_Restaurant.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package com.pcwk.ehr.restaurant;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class C_Restaurant {
+
+
+	public static void addRestaurant(List<C_Restaurant> restaurantList, String filename) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("가게 이름을 입력하세요: ");
+		String resName = scanner.nextLine();
+
+		System.out.print("대표 메뉴를 입력하세요: ");
+		String RemenuName = scanner.nextLine();
+
+		System.out.print("가격을 입력하세요: ");
+		int RMenuCost = Integer.parseInt(scanner.nextLine());
+
+		System.out.print("평점을 입력하세요(예: 4.5) : ");
+		double rating = Double.parseDouble(scanner.nextLine());
+
+		System.out.print("거리를 입력하세요: ");
+		int distance = Integer.parseInt(scanner.nextLine());
+
+		System.out.print("휴무일을 입력하세요: ");
+		String breakDay = scanner.nextLine();
+
+		C_Restaurant restaurant = new C_Restaurant();
+		restaurantList.add(restaurant);
+
+		saveRestaurants(filename, restaurantList);
+		System.out.println("맛집이 등록되었습니다!");
+	}
+
+	private static void saveRestaurants(String filename, List<C_Restaurant> restaurantList) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static List<C_Restaurant> addRestaurant(String file) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getResName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getRmenuName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getRmenuCost() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getRating() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getDistance() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getBreakDay() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
