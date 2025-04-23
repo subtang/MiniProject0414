f1f07529959cf7ac38d9ea5aac833b42d71f6263
YangSeungHyunn <yangsh5972@gmail.com>
2025-04-23 19:25:03 +0900

    삭제 후 저장 기능 추가

--- D_Restaurant.java
+++ D_Restaurant.java
@@ -4,12 +4,12 @@ public class D_Restaurant {
 
 	// 1. 전체 목록 삭제하기
 	public boolean doDelete(String delName) {
-		ReadCSVfile_Edit.loadRestaurants(ReadCSVfile_Edit.Restaurant_DATA); // 콘솔 실행 시 CSV 불러오기 하면 삭제 가능
 		Restaurant removed = ReadCSVfile_Edit.getRestaurantMap().remove(delName);
 
 		if (removed != null) {
 			// List에서도 삭제
 			ReadCSVfile_Edit.getrestaurantList().remove(removed);
+			Save.saveRestaurants(ReadCSVfile_Edit.Restaurant_DATA, ReadCSVfile_Edit.getrestaurantList());
 			return true; // 성공
 		}
 		return false; // 실패
