02e84eb2c72228d0dcc044533f56de36c200de70
subtang <wdh142@gmail.com>
2025-04-24 10:51:36 +0900

    배틀모드 수정

--- Battle.java
+++ Battle.java
@@ -11,38 +11,51 @@ public class B_Restaurant {
 
     //  기존 메서드들 생략
 
-    public static void battleMode(List<Restaurant> restaurantList) {
-        if (restaurantList.size() < 2) {
-            System.out.println("맛집이 2곳 이상 있어야 배틀이 가능합니다.");
-            return;
-        }
+	public static void battleMode(List<Restaurant> restaurantList) {
+	    if (restaurantList.size() < 2) {
+	        System.out.println("맛집이 2곳 이상 있어야 배틀이 가능합니다.");
+	        return;
+	    }
 
-        Random random = new Random();
-        int index1 = random.nextInt(restaurantList.size());
-        int index2;
+	    Scanner scanner = new Scanner(System.in);
+	    Random random = new Random();
 
-        // 중복 방지
-        do {
-            index2 = random.nextInt(restaurantList.size());
-        } while (index1 == index2);
+	    while (true) {
+	        int index1 = random.nextInt(restaurantList.size());
+	        int index2;
 
-        Restaurant r1 = restaurantList.get(index1);
-        Restaurant r2 = restaurantList.get(index2);
+	        do {
+	            index2 = random.nextInt(restaurantList.size());
+	        } while (index1 == index2);
 
-        System.out.print(" 맛집 배틀! 다음 두 곳 중 어디가 더 끌리나요?");
-        System.out.print("\n[1] " + r1);
-        System.out.print("\n[2] " + r2);
+	        Restaurant r1 = restaurantList.get(index1);
+	        Restaurant r2 = restaurantList.get(index2);
 
-        // 선택은 여기선 안 받아도 되고, 필요하면 추가 가능!
-//        Scanner sc = new Scanner(System.in);
-//        int num=sc.nextInt();
-//        if(num==1) {
-//        	
-//        }else if(num==2) {
-//        	
-//        }
-        
-    }
+	        System.out.println("\n맛집 배틀! 다음 두 곳 중 어디가 더 끌리나요?");
+	        System.out.println("[1] " + r1);
+	        System.out.println("[2] " + r2);
+	        System.out.println("[3] 맘에 들지 않으면 다시 뽑기");
+	        System.out.print("선택(1, 2, 3): ");
+
+	        String input = scanner.nextLine();
+
+	        switch (input) {
+	            case "1":
+	                System.out.println("\n선택한 맛집: " + r1);
+	                return;  // 종료
+	            case "2":
+	                System.out.println("\n선택한 맛집: " + r2);
+	                return;  // 종료
+	            case "3":
+	                System.out.println("\n맘에 들지 않으면 다시 뽑으세요!");
+	                // 루프 반복하며 재추첨
+	                break;
+	            default:
+	                System.out.println("잘못된 입력입니다. 다시 선택하세요.");
+	                // 루프 반복하며 재입력
+	        }
+	    }
+	}
 }
 
 
